ea79d94f5774183c8f4a8195b4e98ace89a45039
Md Rasel Shah <mdraselshah56@gmail.com>
2022-04-17 16:11:57 +0600

    404 page create

--- src/Pages/HomePage/HomeData/HomeData.js
+++ src/Pages/HomePage/HomeData/HomeData.js
@@ -4,7 +4,7 @@ import "./HomeData.css";
 const HomeData = ({ singleData }) => {
   const { name, price, description } = singleData;
   return (
-    <div className="service-info cursor-pointer  mt-10 p-8 mb-10 bg-white">
+    <div className="service-info cursor-pointer mt-12 p-8 mb-10 bg-white">
       <h2 className=" text-2xl font-bold mb-3">{name}</h2>
       <p>{description.slice(0, 150) + "..."}</p>
       <p className="text-lg mt-2">$ {price}</p>
--- src/Pages/NotAPage/NotAPage.js
+++ src/Pages/NotAPage/NotAPage.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const NotAPage = () => {
-    return (
-        <div>
-            <h2>404</h2>
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex">
+          <h2 className="text-4xl px-2">404</h2>
+          <div className="bg-amber-400 w-1 h-10"></div>
+          <h2 className="text-4xl px-2">Page not found</h2>
         </div>
-    );
+        <button className="hover:bg-green-600 hover:text-white bg-green-500 py-1 text-lg font-bold text-white mt-10 w-1/6">
+          Back to Home
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default NotAPage;
